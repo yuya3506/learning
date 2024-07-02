024b2a044887f50f7b7cfeee726ea5cd3bd1cee5
Yuya Nagai <ny35060519@gmail.com>
2024-07-02 23:57:48 +0900

    データベース定義、データベース接続処理の変更

--- blog_app/app/src/index.php
+++ blog_app/app/src/index.php
@@ -8,9 +8,12 @@ $password = 'test';
 
 try {
     $pdo = new PDO($dsn, $user, $password);
-    $sth = $pdo->query("SELECT * FROM users WHERE id = 1");
+    $sth = $pdo->query("SELECT * FROM blog ORDER BY id DESC LIMIT 5");
     $user = $sth->fetch(PDO::FETCH_ASSOC);
     var_dump($user);
+    echo '接続成功';
+    # 接続終了
+    $pdo = null;
 } catch (PDOException $e) {
     print('Error:'.$e->getMessage());
     exit;
--- blog_app/mysql/initdb.d/init.sql
+++ blog_app/mysql/initdb.d/init.sql
@@ -1,8 +1,8 @@
-CREATE TABLE test_db.users (
-    id          INT             NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    age         INT,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO `users` VALUES (1, 'Yuya', 23)
+CREATE TABLE test_db.blog (
+    id             INT           NOT NULL PRIMARY KEY,
+    title          VARCHAR(191)  NOT NULL,
+    content        INT           NOT NULL,
+    category       INT           NOT NULL,
+    post_at        TIMESTAMP     NOT NULL,
+    publish_status INT           NOT NULL DEFAULT '1'
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
