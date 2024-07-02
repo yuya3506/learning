d1aedb28000431f726e59ba7a1d13f69e1d36a6c
yuya3506 <ny35060519@gmail.com>
2024-07-02 22:25:54 +0900

    Delete Docker directory

--- Docker/mysql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE test_db.users (
-    id          INT             NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    age         INT,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO `users` VALUES (1, 'Yuya', 23)
